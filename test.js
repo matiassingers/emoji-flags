99587fbdc8b38610168cf7eb186ad74951e49add
Laura Carvajal <laura.carvajal@gmail.com>
2016-07-18 11:45:35 +0100

    add EU flag 🇪🇺

--- test.js
+++ test.js
@@ -5,34 +5,34 @@ var emojiFlags = require('./');
 
 describe('getter methods', function(){
   it('data', function() {
-    assert.equal(emojiFlags.data.length, 249);
+    assert.equal(emojiFlags.data.length, 250);
 
 
     assert.equal(emojiFlags.data[2].name, 'Afghanistan');
   });
 
   it('emojis', function() {
-    assert.equal(emojiFlags.emojis.length, 249);
+    assert.equal(emojiFlags.emojis.length, 250);
 
-    assert.equal(emojiFlags.emojis[76], '🇬🇧');
+    assert.equal(emojiFlags.emojis[77], '🇬🇧');
   });
 
   it('codes', function() {
-    assert.equal(emojiFlags.codes.length, 249);
+    assert.equal(emojiFlags.codes.length, 250);
 
-    assert.equal(emojiFlags.codes[200], 'SJ');
+    assert.equal(emojiFlags.codes[201], 'SJ');
   });
 
   it('names', function() {
-    assert.equal(emojiFlags.names.length, 249);
+    assert.equal(emojiFlags.names.length, 250);
 
-    assert.equal(emojiFlags.names[150], 'Mauritania');
+    assert.equal(emojiFlags.names[151], 'Mauritania');
   });
 
   it('unicodes', function() {
-    assert.equal(emojiFlags.unicodes.length, 249);
+    assert.equal(emojiFlags.unicodes.length, 250);
 
-    assert.equal(emojiFlags.unicodes[101], 'U+1F1EE U+1F1EA');
+    assert.equal(emojiFlags.unicodes[102], 'U+1F1EE U+1F1EA');
   });
 
   it('country code getters', function() {
