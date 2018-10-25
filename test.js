0dea80890732754986b83523061b88bd243e90a4
Eric Chen <fshiori@gmail.com>
2018-10-25 14:13:19 +0800

    <fix>(test) fix unittest

--- test.js
+++ test.js
@@ -5,32 +5,32 @@ var emojiFlags = require('./');
 
 describe('getter methods', function(){
   it('data', function() {
-    assert.equal(emojiFlags.data.length, 250);
+    assert.equal(emojiFlags.data.length, 251);
 
 
     assert.equal(emojiFlags.data[2].name, 'Afghanistan');
   });
 
   it('emojis', function() {
-    assert.equal(emojiFlags.emojis.length, 250);
+    assert.equal(emojiFlags.emojis.length, 251);
 
     assert.equal(emojiFlags.emojis[77], '🇬🇧');
   });
 
   it('codes', function() {
-    assert.equal(emojiFlags.codes.length, 250);
+    assert.equal(emojiFlags.codes.length, 251);
 
     assert.equal(emojiFlags.codes[201], 'SJ');
   });
 
   it('names', function() {
-    assert.equal(emojiFlags.names.length, 250);
+    assert.equal(emojiFlags.names.length, 251);
 
     assert.equal(emojiFlags.names[151], 'Mauritania');
   });
 
   it('unicodes', function() {
-    assert.equal(emojiFlags.unicodes.length, 250);
+    assert.equal(emojiFlags.unicodes.length, 251);
 
     assert.equal(emojiFlags.unicodes[102], 'U+1F1EE U+1F1EA');
   });
